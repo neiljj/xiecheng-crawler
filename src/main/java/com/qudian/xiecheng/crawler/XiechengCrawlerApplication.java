592c52400ed1edb8304c297e3ebca28349891718
倪继昌 <nijichang@qudian.com>
2020-10-30 16:09:24 +0800

    基础类添加2

--- src/main/java/com/qudian/xiecheng/crawler/XiechengCrawlerApplication.java
+++ src/main/java/com/qudian/xiecheng/crawler/XiechengCrawlerApplication.java
@@ -1,11 +1,15 @@
 package com.qudian.xiecheng.crawler;
 
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 @Slf4j
+@EnableConfigurationProperties
+@MapperScan("com.qudian.xiecheng.crawler.dao")
 public class XiechengCrawlerApplication {
 
     public static void main(String[] args) {
