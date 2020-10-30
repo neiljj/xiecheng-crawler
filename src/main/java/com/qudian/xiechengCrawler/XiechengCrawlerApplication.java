d99f0ad07b20ef64e90eaaa9e4c852a391f41b80
倪继昌 <nijichang@qudian.com>
2020-10-30 11:12:57 +0800

    Initial commit

---
+++ src/main/java/com/qudian/xiechengCrawler/XiechengCrawlerApplication.java
@@ -0,0 +1,16 @@
+package com.qudian.xiechengCrawler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class XiechengCrawlerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiechengCrawlerApplication.class, args);
+        log.info("爬虫程序已启动");
+    }
+
+}
---
+++ src/test/java/com/qudian/xiechengCrawler/NijichangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qudian.xiechengCrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NijichangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
