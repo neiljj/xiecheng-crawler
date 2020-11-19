53219aab04f7b2cf2562b9b5967ec090f08b5f27
倪继昌 <nijichang@qudian.com>
2020-11-19 16:17:16 +0800

    优化

--- src/main/java/com/xiecheng/crawler/service/biz/AbstractCrawlerBiz.java
+++ src/main/java/com/xiecheng/crawler/service/biz/AbstractCrawlerBiz.java
@@ -25,7 +25,7 @@ public abstract class AbstractCrawlerBiz {
 
     public void await(){
         try {
-            Thread.sleep(3000);
+            Thread.sleep(5000);
         }catch (InterruptedException e){
             log.error(e.getMessage());
         }
